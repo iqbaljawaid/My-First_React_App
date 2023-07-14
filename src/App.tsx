5521822d2e27a4fd390e4c7c2dd3a41411360f22
Iqbal Jawaid <128409931+iqbaljawaid@users.noreply.github.com>
2023-07-14 18:18:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Calculator from "./Calculator";
-import CalculatorFunc from './CalculatorFuncComponent';
+
 
 function App() {
   return (
     <div className="App">
       <Calculator/>
-      <hr/>
-      <br/>
-      <CalculatorFunc/>
-    </div>
+      
   );
 }
 
